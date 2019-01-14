cff69941bc0cac77fcb7fc692ce93cfb4f484b70
abstract-animation-9914 <vm930@hotmail.com>
2019-01-14 22:40:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+ if block_given?
+ i = 0
+ new_array = []
+ while i <  array.length
+ new_array.push (yield array[i])
+ i = i + 1
+  end
+end
+  new_array
+end
 
-
+ 
+   #{|name| name.split(" ").first}
+##else
+    ##{|lang|lang.upcase}
